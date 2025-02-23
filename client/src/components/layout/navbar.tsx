dde8b8495f5d6fcb86746af116001ca94051a5b5
KingEknows <11398490-KingEknows@users.noreply.replit.com>
2025-02-23 04:22:58 +0000

    Agent query: Is the new transparent logo showing up correctly in the top left of the website?
    Update website logo to remove background
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/7268d2c9-b27d-4bc5-9343-55b8aff5eca0/a9aaed07-94d5-4486-8161-54332a7dcdc1.jpg

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
               <Link href="/">
                 <Button variant="ghost" className="p-0">
                   <img 
-                    src="/IMG_2055.jpeg" 
+                    src="/IMG_2055-removebg-preview.png" 
                     alt="Pure Game Classic Logo" 
                     className="h-8 w-auto"
                   />
